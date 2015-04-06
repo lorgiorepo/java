593129fec801effdd3560eee4f5501adc9c66e6a
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2015-04-05 23:02:43 -0300

    Configuracion de liquibase

---
+++ liquibase-for-all/src/main/resources/liquibase/sql/1/carga-entidades.sql
@@ -0,0 +1,5 @@
+insert into ENTIDAD(PK_ENTIDAD, NOMBRE, FECHA_CREACION) values (1, 'Ministerio de transportes', now());
+insert into ENTIDAD(PK_ENTIDAD, NOMBRE, FECHA_CREACION) values (2, 'Ministerio del trabajo', now());
+insert into ENTIDAD(PK_ENTIDAD, NOMBRE, FECHA_CREACION) values (3, 'Ministerio de educacion', now());
+insert into ENTIDAD(PK_ENTIDAD, NOMBRE, FECHA_CREACION) values (4, 'Contraloria', now());
+insert into ENTIDAD(PK_ENTIDAD, NOMBRE, FECHA_CREACION) values (5, 'Ministerio del interior', now());
